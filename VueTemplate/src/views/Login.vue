8b83ecd50c9789826d679a81b95b919d8ac3daae
justinzhang <justin.zhang@unition.global>
2024-05-15 14:30:28 +0800

    add ignore path and reformat

--- VueTemplate/src/views/Login.vue
+++ VueTemplate/src/views/Login.vue
@@ -2,8 +2,13 @@
     <div class="login">
         <div class="box">
             <h2>Login</h2>
-            <el-form ref="formRef" :model="validateForm" :rules="inputRules" label-width="100px" class="demo-ruleForm">
-
+            <el-form
+                ref="formRef"
+                :model="validateForm"
+                :rules="inputRules"
+                label-width="100px"
+                class="demo-ruleForm"
+            >
                 <el-form-item label="Email" prop="email">
                     <el-input type="text" v-model="validateForm.email" />
                 </el-form-item>
@@ -13,94 +18,96 @@
                 </el-form-item>
 
                 <el-form-item>
-                    <el-button type="primary" @click="submitForm(formRef)">Submit</el-button>
+                    <el-button type="primary" @click="submitForm(formRef)"
+                        >Submit</el-button
+                    >
                     <el-button @click="resetForm(formRef)">Reset</el-button>
-                    <el-button type="danger" link @click="goToRegisterPage">No account? Register now!</el-button>
+                    <el-button type="danger" link @click="goToRegisterPage"
+                        >No account? Register now!</el-button
+                    >
                 </el-form-item>
-
             </el-form>
         </div>
     </div>
 </template>
 
 <script lang="ts" setup name="Login">
-    import { reactive, ref } from 'vue'
-    import type { FormInstance } from 'element-plus'
-    import { ElNotification } from 'element-plus'
-    import { $post } from '@/utils/request';
-    import router from '@/router';
+import { reactive, ref } from "vue";
+import type { FormInstance } from "element-plus";
+import { ElNotification } from "element-plus";
+import { $post } from "@/utils/request";
+import router from "@/router";
 
-    const formRef = ref<FormInstance>()
+const formRef = ref<FormInstance>();
 
-    const validateForm = reactive({
-        email: '',
-        password: '',
-    })
+const validateForm = reactive({
+    email: "",
+    password: "",
+});
 
-    const inputRules = {
-        email: [
-            {
-                required: true,
-                message: 'Please input email address',
-                trigger: 'blur',
-            },
-            {
-                type: 'email',
-                message: 'Please input correct email address',
-                trigger: ['blur', 'change'],
-            },
-        ],
-        password: [
-            {
-                required: true,
-                message: 'Please input password',
-                trigger: 'blur',
-            },
-            {
-                min: 6,
-                message: 'Password length must be at least 6 characters',
-                trigger: 'blur',
-            }
-        ],
-    }
+const inputRules = {
+    email: [
+        {
+            required: true,
+            message: "Please input email address",
+            trigger: "blur",
+        },
+        {
+            type: "email",
+            message: "Please input correct email address",
+            trigger: ["blur", "change"],
+        },
+    ],
+    password: [
+        {
+            required: true,
+            message: "Please input password",
+            trigger: "blur",
+        },
+        {
+            min: 6,
+            message: "Password length must be at least 6 characters",
+            trigger: "blur",
+        },
+    ],
+};
 
-    const alert = (title:string,message:string,type:string) => {
-        ElNotification({
-            title: title,
-            message: message,
-            type: type,
-        })
-    }
+const alert = (title: string, message: string, type: string) => {
+    // ElNotification({
+    //     title: title,
+    //     message: message,
+    //     type: type,
+    // })
+};
 
-    const submitForm = (formEl: FormInstance | undefined) => {
-        if (!formEl) return
-        formEl.validate((valid) => {
-            if (valid) {
-                $post('/Admin/Login', validateForm)
-                .then((response) => {
-                    if(response.data.errorCode === 0){
-                        alert('Success','Login Success','success')
-                        router.push('/customerList')
-                    }else{
-                        alert('Warning',response.data.errorMessage,'warning')
-                    }
-                })
-            }
-        })
-    }
+const submitForm = (formEl: FormInstance | undefined) => {
+    if (!formEl) return;
+    formEl.validate((valid) => {
+        if (valid) {
+            $post("/Admin/Login", validateForm).then((response) => {
+                if (response.data.errorCode === 0) {
+                    alert("Success", "Login Success", "success");
+                    router.push("/customerList");
+                } else {
+                    alert("Warning", response.data.errorMessage, "warning");
+                }
+            });
+        }
+    });
+};
 
-    const resetForm = (formEl: FormInstance | undefined) => {
-        if (!formEl) return
-        formEl.resetFields()
-    }
+const resetForm = (formEl: FormInstance | undefined) => {
+    if (!formEl) return;
+    formEl.resetFields();
+};
 
-    const goToRegisterPage = () => {
-        router.push('/register')
-    }
+const goToRegisterPage = () => {
+    router.push("/register");
+};
 </script>
 
 <style scoped lang="scss">
-.login{
+.login {
     width: 100vw;
     height: 100vh;
     background: white;
@@ -109,14 +116,14 @@
     align-items: center;
     background-image: url(../images/background.png);
     background-size: cover;
-    .box{
+    .box {
         width: 500px;
         margin: 0 auto;
         padding: 20px;
         border-radius: 5px;
-        box-shadow: 0 0 10px rgba(0,0,0,.5);
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
         background: white;
-        h2{
+        h2 {
             font-size: 20px;
             text-align: center;
             margin-bottom: 20px;
--- VueTemplate/src/views/Register.vue
+++ VueTemplate/src/views/Register.vue
@@ -2,8 +2,13 @@
     <div class="register">
         <div class="box">
             <h2>Register New User</h2>
-            <el-form ref="formRef" :model="validateForm" :rules="inputRules" label-width="140px" class="demo-ruleForm">
-
+            <el-form
+                ref="formRef"
+                :model="validateForm"
+                :rules="inputRules"
+                label-width="140px"
+                class="demo-ruleForm"
+            >
                 <el-form-item label="Email" prop="email">
                     <el-input type="text" v-model="validateForm.email" />
                 </el-form-item>
@@ -13,7 +18,10 @@
                 </el-form-item>
 
                 <el-form-item label="Confirm Password" prop="confirmPassword">
-                    <el-input type="password" v-model="validateForm.confirmPassword" />
+                    <el-input
+                        type="password"
+                        v-model="validateForm.confirmPassword"
+                    />
                 </el-form-item>
 
                 <el-form-item label="Phone" prop="phone">
@@ -25,130 +33,136 @@
                 </el-form-item>
 
                 <el-form-item>
-                    <el-button type="primary" @click="submitForm(formRef)">Submit</el-button>
+                    <el-button type="primary" @click="submitForm(formRef)"
+                        >Submit</el-button
+                    >
                     <el-button @click="resetForm(formRef)">Reset</el-button>
-                    <el-button type="danger" link @click="goToLoginPage">Already have account? Login now!</el-button>
+                    <el-button type="danger" link @click="goToLoginPage"
+                        >Already have account? Login now!</el-button
+                    >
                 </el-form-item>
-
             </el-form>
         </div>
     </div>
 </template>
 
 <script lang="ts" setup name="Register">
-    import { reactive, ref } from 'vue'
-    import type { FormInstance } from 'element-plus'
-    import { ElNotification } from 'element-plus'
-    import { $post } from '@/utils/request';
-    import router from '@/router';
+import { reactive, ref } from "vue";
+import type { FormInstance } from "element-plus";
+import { ElNotification } from "element-plus";
+import { $post } from "@/utils/request";
+import router from "@/router";
 
-    const formRef = ref<FormInstance>()
+const formRef = ref<FormInstance>();
 
-    const validateForm = reactive({
-        email: '',
-        password: '',
-        confirmPassword: '',
-        phone: '',
-        address: '',
-    })
+const validateForm = reactive({
+    email: "",
+    password: "",
+    confirmPassword: "",
+    phone: "",
+    address: "",
+});
 
-    const validateConfirmPassword = (rule: any, value: string, callback: Function) => {
-        if (value !== validateForm.password) {
-            callback(new Error('The two passwords do not match'));
-        } else {
-            callback();
+const validateConfirmPassword = (
+    rule: any,
+    value: string,
+    callback: Function,
+) => {
+    if (value !== validateForm.password) {
+        callback(new Error("The two passwords do not match"));
+    } else {
+        callback();
+    }
+};
+
+const inputRules = {
+    email: [
+        {
+            required: true,
+            message: "Please input email address",
+            trigger: "blur",
+        },
+        {
+            type: "email",
+            message: "Please input correct email address",
+            trigger: ["blur", "change"],
+        },
+    ],
+    password: [
+        {
+            required: true,
+            message: "Please input password",
+            trigger: "blur",
+        },
+        {
+            min: 6,
+            message: "Password length must be at least 6 characters",
+            trigger: "blur",
+        },
+    ],
+    confirmPassword: [
+        {
+            required: true,
+            message: "Please input password",
+            trigger: "blur",
+        },
+        {
+            validator: validateConfirmPassword,
+            trigger: "blur",
+        },
+    ],
+    phone: [
+        {
+            required: true,
+            message: "Please input phone",
+            trigger: "blur",
+        },
+    ],
+    address: [
+        {
+            required: true,
+            message: "Please input address",
+            trigger: "blur",
+        },
+    ],
+};
+
+const alert = (title: string, message: string, type: string) => {
+    // ElNotification({
+    //     title: title,
+    //     message: message,
+    //     type: type,
+    // });
+};
+
+const submitForm = (formEl: FormInstance | undefined) => {
+    if (!formEl) return;
+    formEl.validate((valid) => {
+        if (valid) {
+            $post("/Admin/Register", validateForm).then((response) => {
+                if (response.data.errorCode === 0) {
+                    alert("Success", "Register Success", "success");
+                    router.push("/login");
+                } else {
+                    alert("Warning", response.data.errorMessage, "warning");
+                }
+            });
         }
-    }
+    });
+};
 
-    const inputRules = {
-        email: [
-            {
-                required: true,
-                message: 'Please input email address',
-                trigger: 'blur',
-            },
-            {
-                type: 'email',
-                message: 'Please input correct email address',
-                trigger: ['blur', 'change'],
-            },
-        ],
-        password: [
-            {
-                required: true,
-                message: 'Please input password',
-                trigger: 'blur',
-            },
-            {
-                min: 6,
-                message: 'Password length must be at least 6 characters',
-                trigger: 'blur',
-            }
-        ],
-        confirmPassword: [
-            {
-                required: true,
-                message: 'Please input password',
-                trigger: 'blur',
-            },
-            {
-                validator: validateConfirmPassword,
-                trigger: 'blur',
-            }
-        ],
-        phone: [
-            {
-                required: true,
-                message: 'Please input phone',
-                trigger: 'blur',
-            }
-        ],
-        address: [
-            {
-                required: true,
-                message: 'Please input address',
-                trigger: 'blur',
-            }
-        ],
-    }
+const resetForm = (formEl: FormInstance | undefined) => {
+    if (!formEl) return;
+    formEl.resetFields();
+};
 
-    const alert = (title:string,message:string,type:string) => {
-        ElNotification({
-            title: title,
-            message: message,
-            type: type,
-        })
-    }
-
-    const submitForm = (formEl: FormInstance | undefined) => {
-        if (!formEl) return
-        formEl.validate((valid) => {
-            if (valid) {
-                $post('/Admin/Register', validateForm)
-                .then((response) => {
-                    if(response.data.errorCode === 0){
-                        alert('Success','Register Success','success')
-                        router.push('/login')
-                    }else{
-                        alert('Warning',response.data.errorMessage,'warning')
-                    }
-                })
-            }
-        })
-    }
-
-    const resetForm = (formEl: FormInstance | undefined) => {
-        if (!formEl) return
-        formEl.resetFields()
-    }
-
-    const goToLoginPage = () => {
-        router.push('/login')
-    }
+const goToLoginPage = () => {
+    router.push("/login");
+};
 </script>
 
 <style scoped lang="scss">
-.register{
+.register {
     width: 100vw;
     height: 100vh;
     background: white;
@@ -157,14 +171,14 @@
     align-items: center;
     background-image: url(../images/background.png);
     background-size: cover;
-    .box{
+    .box {
         width: 600px;
         margin: 0 auto;
         padding: 20px;
         border-radius: 5px;
-        box-shadow: 0 0 10px rgba(0,0,0,.5);
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
         background: white;
-        h2{
+        h2 {
             font-size: 20px;
             text-align: center;
             margin-bottom: 20px;
